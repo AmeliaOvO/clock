671fce43dda7c931a238fe8f4de26185e212d13e
AmeliaOvO <amyjxu@outlook.com>
2022-08-27 10:14:57 -0700

    set up working space

---
+++ src/fpga_wrap.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/26/2022 11:17:12 PM
+// Design Name: 
+// Module Name: fpga_wrap
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fpga_wrap(
+    input logic CLK100, 
+    input logic [15:0] SW, 
+    output logic [15:0] LED, 
+    output logic [7:0] AN,
+
+    );
+endmodule
